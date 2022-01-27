6df2b112e42b154ea6e4ffb7dbb2f9960f6ec493
aidisakovic <aid.isakovic@bulme.at>
2022-01-27 13:20:22 +0100

    Name

--- gpio.cpp
+++ gpio.cpp
@@ -1,6 +1,43 @@
 #include "gpio.h"
+#include <lgpio.h>
 
-Gpio::Gpio(QObject *parent) : QObject(parent)
+Gpio::Gpio(QObject parent) : QObject(parent)
+{
+    // Zugriff auf die Gpio Pins kriegen
+    m_handle = lgGpiochipOpen(0);
+    int init_lvl = 0;
+    for(int pin : LEDS)
+        lgGpioClaimOutput(m_handle, LFLAGS, pin, init_lvl); / initial level auf init_lvl setzen /
+}
+
+Gpio::~Gpio()
 {
 
+    int init_lvl = 0;
+    for(int pin : LEDS)
+        lgGpioWrite(m_handle, pin, init_lvl); / initial level = init_lvl /
+
+    lgGpiochipClose(m_handle); // Bei uns optional gibt chip frei
+
+
+}
+
+void Gpio::set(int pin,bool value)
+{
+    lgGpioWrite(m_handle, pin, value); / initial level = init_lvl */
+}
+
+void Gpio::set(unsigned int pattern)
+{
+    // Lauflicht Funktion
+    int n = 0;
+    bool value;
+    unsigned int check = 0b0001;
+    for(auto pin : LEDS)
+    {
+        // n stelle von pattern ausmaskieren, value =
+        value = check & pattern>>n;
+        lgGpioWrite(m_handle, pin, value);
+        n++;
+    }
 }
